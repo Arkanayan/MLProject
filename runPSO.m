612b8e2fd8a2ecf785a14faf0467f97e7850610c
Arka <itsarkanayan@gmail.com>
2016-11-16 12:28:41 +0530

    Extract run and write pso to single file

---
+++ runPSO.m
@@ -0,0 +1,13 @@
+
+result_filename = 'results.xlsx';
+
+data = load('slump_test.data');
+
+X = [ data(:, 2:7) data(:, 9:11)   ];
+
+y = data(:, 8);
+
+Xnorm = normalze(X);
+ynorm = normalze(y);
+
+[status] = runAndReportPSO(Xnorm, ynorm, 3, result_filename, 'C5');
