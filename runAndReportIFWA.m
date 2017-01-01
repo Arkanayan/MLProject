a522de693863a44b530a800babae93e75b4d5066
Arka Nayan <itsarkanayan@gmail.com>
2017-01-01 21:10:13 +0530

    Add files to run IFWA

---
+++ runAndReportIFWA.m
@@ -0,0 +1,69 @@
+function [ status ] = runAndReportFWA(X, y, de_iter, no_of_iter, fileName, start_cell)
+%RUNANDREPORT Run PSO and write it to excel
+%   Detailed explanation goes here
+
+% xlswrite(filename, run_testing_error, 'Testing', 'C5');
+
+TEST_RUN=10;
+
+N=size(X,1);
+P=0.2;
+
+Xnorm = X;
+ynorm = y;
+
+num_of_iter = 3;
+
+result_testing_error = [];
+result_training_error = [];
+
+if ~exist('de_iter', 'var')
+    de_iter = 200
+end
+
+for j = 1:1:num_of_iter
+
+for i = 1:1:TEST_RUN
+
+[Train, Test] = crossvalind('HoldOut', N, P);
+
+[theta, costHist] = IFWA(Xnorm, ynorm, 5);
+
+
+
+%--Training error
+data_size = size( Xnorm(Train,:), 1 );
+
+Y = Xnorm(Train,:) * theta';
+
+Error=sum( abs( ynorm(Train) - Y ) );
+
+run_training_error(i,1) = Error;
+
+%--Training error
+data_size = size(Xnorm(Test,:), 1);
+
+Y = Xnorm(Test, :) * theta'; 
+
+Error = sum( abs( ynorm(Test) - Y ) );
+
+run_testing_error(i,1) = Error;
+
+
+fprintf('Error = %f\n', Error);
+
+
+end
+
+result_testing_error = [ result_testing_error ; run_testing_error];
+result_training_error = [ result_training_error ; run_training_error];
+end
+
+% [status_test] = xlswrite(fileName, result_testing_error, 'Testing', start_cell);
+% [status_train] = xlswrite(fileName, result_training_error, 'Training', start_cell);
+% 
+% status = status_test && status_train;
+status = 1
+% End of function
+end
+
